a55669fdf11d342a802711d9257fb9f3bc56485e
Joonas Honkavaara <joonas.honkavaara@hotmail.com>
2025-12-07 17:24:18 +0200

    Connetion from backend to frontend

--- code/frontend/src/api.ts
+++ code/frontend/src/api.ts
@@ -1,14 +1,32 @@
 export const fetchInitialDocument = async () => {
-  // todo: replace with a real GET request to server
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  return 'Initial document text from server'
+  const response = await fetch("http://127.0.0.1:8000/test-db")
+  const data = await response.json()
+  if (data.notes) {
+    return data.notes
+      .map((n: any) => `Title: ${n.title}\nContent: ${n.content}`)
+      .join("\n\n")
+  }
+
+  return ""
 }
 
 export const connectSocket = (onMessage: (value: string) => void) => {
-  const socket = new WebSocket('ws://127.0.0.1:800') // todo: replace with real server URL
+  const socket = new WebSocket('ws://localhost:8000/ws') 
 
   socket.addEventListener('message', (event) => {
-    onMessage(String(event.data ?? ''))
+    try {
+      const msg = JSON.parse(event.data);
+
+      if (msg.notes) {
+        const text = msg.notes
+          .map((n: any) => `Title: ${n.title}\nContent: ${n.content}`)
+          .join("\n\n");
+
+        onMessage(text);
+      }
+    } catch (err) {
+      console.error("Invalid JSON message:", event.data);
+    }
   })
 
   socket.addEventListener('open', () => {
--- code/server/server.py
+++ code/server/server.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient("mongodb://mongodb:27017")
 db = client["mydatabase"]
@@ -23,4 +37,12 @@ def test_db():
         "notes": all_notes
     }
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    all_notes = list(notes.find({}, {"_id": 0}))
+    await websocket.send_json({"notes": all_notes})
+
+    while True:
+        await websocket.receive_text()
 
